c48d7ac99a90fd3b6d7969eaa1a6df0358bf227a
JooChan <14jshin@gmail.com>
2020-01-20 23:41:07 -0500

    trying to reduce the numbers to solve

--- optimal-solve.py
+++ optimal-solve.py
@@ -9,17 +9,25 @@ rand = random.randrange(0, 1029362)
 with open("/Users/joochanshin/Desktop/htm.txt") as f:
     for line in f:
         counter += 1
-        algList.append(line)
+        if line[0] + line[1] == 'R1':
+            for i in range(0,len(line)):
+                if i%2 == 1:
+                    command = command + line[i]
+                    commands.append(command)
+                    command = ""
+                else:
+                    command = command + line[i]
+            algList.append(commands)
+            commands = []
+            command = ""
 
-for i in range(0, len(algList[rand])):
-    if i%2 == 1:
-        command = command + algList[rand][i]
-        commands.append(command)
-        command = ""
-    else:
-        command = command + algList[rand][i]
+# for i in range(0, len(algList[rand])):
+#     if i%2 == 1:
+#         command = command + algList[rand][i]
+#         commands.append(command)
+#         command = ""
+#     else:
+#         command = command + algList[rand][i]
+rand2 = random.randrange(0, len(algList))
+print(algList[rand2], rand2, len(algList))
 
-print(commands)
-
-for i in range(0, len(commands)):
-    print(commands[i], 19-i)
